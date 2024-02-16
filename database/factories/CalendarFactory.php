3b832eef00ea6e36acd575970abc85b3685ae263
Delpech <juliette.delpech@gmail.com>
2024-02-16 16:04:41 +0100

    db

--- database/factories/CalendarFactory.php
+++ database/factories/CalendarFactory.php
@@ -13,7 +13,11 @@ class CalendarFactory extends Factory
         return [
             'location' => $this->faker->word(),
             'fr_city' => $this->faker->word(),
+            'en_city' => $this->faker->word(),
+            'nl_city' => $this->faker->word(),
             'fr_dateConcert' => $this->faker->word(),
+            'en_dateConcert' => $this->faker->word(),
+            'nl_dateConcert' => $this->faker->word(),
             'orderElt' => $this->faker->randomDigit(),
             'link' => $this->faker->url(),
         ];
