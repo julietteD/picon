4fbca69d0f6c8242b8b25e7f29e2409b8abecc58
Delpech <juliette.delpech@gmail.com>
2024-03-04 13:56:11 +0100

    js

--- public/js/front.js
+++ public/js/front.js
@@ -10,8 +10,3 @@ ScrollTrigger.create({
     start: "top center",
     trigger: "#section2",
 });
-/*
-const elem = document.querySelector('input[name="birthdate"]');
-const datepicker = new Datepicker(elem, {
-    format: 'dd-mm-yyyy',
-});*/
