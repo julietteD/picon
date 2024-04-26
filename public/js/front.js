ed49d3ee00939a3ac18e581a23a8bdbea26d1fe1
Delpech <juliette.delpech@gmail.com>
2024-04-26 08:34:00 +0200

    video after movie

--- public/js/front.js
+++ public/js/front.js
@@ -1,8 +1,8 @@
 const tl = gsap.timeline();
 const tl2 = gsap.timeline();
 
-tl2.to(".content-text h2", { opacity: 1 }).to(".content-text .programme", { opacity: 1 }).to(".content-text a", { opacity: 1 })
-tl.to(".showcase-pict", { opacity: 1 }).to(".showcase-form", { opacity: 1 })
+/*tl2.to(".content-text h2", { opacity: 1 }).to(".content-text .programme", { opacity: 1 }).to(".content-text a", { opacity: 1 })
+tl.to(".showcase-pict", { opacity: 1 }).to(".showcase-form", { opacity: 1 })*/
 
 
 ScrollTrigger.create({
@@ -12,7 +12,7 @@ ScrollTrigger.create({
 });
 
 
-const elem = document.querySelector('input[name="birthdate"]');
+/*const elem = document.querySelector('input[name="birthdate"]');
 const datepicker = new Datepicker(elem, {
     format: 'dd-mm-yyyy',
-});
+});*/
